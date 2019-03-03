0ab09d51e8fda750899e7be6bff9b793b117b11b
Peter de Croos <cultofmetatron@aumlogic.com>
2019-03-02 20:06:09 -0500

    added to gl vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[allow(dead_code)]
+struct GlPoint {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl GlPoint {
+    #[allow(dead_code)]
+    fn new(x: f64, y: f64, z: f64) -> GlPoint {
+        GlPoint {
+            x,
+            y,
+            z
+        }
+    }
+}
+
+#[allow(dead_code)]
+struct GlVector {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl GlVector {
+    #[allow(dead_code)]
+    fn new(x: f64, y: f64, z: f64) -> GlVector {
+        GlVector {
+            x,
+            y,
+            z
+        }
+    }
+}
