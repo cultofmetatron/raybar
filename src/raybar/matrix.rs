6e6d934edb6dc8b75ea60ab319a4c181cf3dc0d9
Peter de Croos <cultofmetatron@aumlogic.com>
2019-06-14 18:39:06 -0400

    ran formatter

--- src/raybar/matrix.rs
+++ src/raybar/matrix.rs
@@ -226,45 +226,35 @@ impl<
     pub fn invert(&self) -> Option<GlMatrix<f64>> {
         let size = self.get_col_size();
         let det = self.det(); //determing the deterinate
-        // a zero determineate indicates there is no inverse to be had
+                              // a zero determineate indicates there is no inverse to be had
         if self.is_square() {
             Option::None
         } else if det == Zero::zero() {
             Option::None
         } else {
             let mut invert_contents: Vec<Vec<f64>> = (0..size)
-                .map(|_row| {
-                    (0..size)
-                    .map(|_val| 0.0)
-                    .collect()
-                })
+                .map(|_row| (0..size).map(|_val| 0.0).collect())
                 .collect();
             let floating_det = det.to_f64().unwrap();
             for row in 0..size {
                 for col in 0..size {
-                    invert_contents[col][row] = self.cofactor(row, col)
+                    invert_contents[col][row] = self
+                        .cofactor(row, col)
                         .to_f64()
-                        .map(|cofactor| {
-                            cofactor / floating_det
-                        })
+                        .map(|cofactor| cofactor / floating_det)
                         .unwrap()
                 }
             }
 
-
             Option::Some(GlMatrix::new(invert_contents))
         }
-        
     }
     #[allow(dead_code)]
     pub fn mult(&self, scaler: T) -> GlMatrix<T> {
-        let contents = self.content.iter()
-            .map(|row| {
-                row.iter().map(|val| {
-                    *val * scaler
-                })
-                .collect()
-            })
+        let contents = self
+            .content
+            .iter()
+            .map(|row| row.iter().map(|val| *val * scaler).collect())
             .collect();
         GlMatrix::new(contents)
     }
@@ -273,13 +263,13 @@ impl<
     */
     #[allow(dead_code)]
     pub fn to_floats(&self) -> GlMatrix<f64> {
-        let contents = self.content.iter()
+        let contents = self
+            .content
+            .iter()
             .map(|row| {
                 row.iter()
-                .map(|val| {
-                    (*val).to_f64().unwrap_or(0.0)
-                })
-                .collect()
+                    .map(|val| (*val).to_f64().unwrap_or(0.0))
+                    .collect()
             })
             .collect();
         GlMatrix::new(contents)
